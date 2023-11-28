b290d5aeb20dfbbfb42f0b3a9234a667c8c504a5
larswmat@gmail.com <larswmat@gmail.com>
2023-11-28 12:17:01 +0100

    enemy code cleaned up and moved around so its more oraganizezd

--- main.lua
+++ main.lua
@@ -23,20 +23,14 @@ local player = {
     move = false;
 }
 
-local enemy = {
-    x = 10,
-    y = 10,
-    health = 1,
-    speed = 25,
-    word = 'boom'
-}
 
+--ENEMY SETTINGS
 local enemyList = {}
 local enemySpawnTimer = 5
 local prevSpawnTime = 0
 local spawnTimeDecliner = 5
 local prevDecilineTime = 0
-
+--------------------------
 
 function love.load()
     print("lets go")
@@ -57,35 +51,12 @@ function love.load()
 
     -- enable key repeat so backspace can be held down to trigger love.keypressed multiple times.
     love.keyboard.setKeyRepeat(true)
+
+    --spawn first enemy
+    SpawnEnemy()
    
 end
-function ManageEnemies(dt)
-    local currentTime = love.timer.getTime()
-    local updatedSpawnTime = false
-  
-    -- Spawn enemy every nth second
-    if currentTime - prevSpawnTime >= enemySpawnTimer then
-        print(enemySpawnTimer .. " has passed. Spawn enemy")
-        prevSpawnTime = currentTime
-        SpawnEnemy()
-        updatedSpawnTime = true
-    end
-  
-    -- Decrement spawn time if necessary
-    if enemySpawnTimer > 0.5 and not updatedSpawnTime then
-        if currentTime - prevDecilineTime >= spawnTimeDecliner then
-            prevDecilineTime = currentTime
-            enemySpawnTimer = enemySpawnTimer - 0.5
-            print('Decline spawn time with 0.5, new spawn time: ' .. enemySpawnTimer)
-        end
-    end
-  
-    -- Move enemies towards the player
-    MoveTowards(enemy, player.x, player.y, dt)
-    for index, value in ipairs(enemyList) do
-      MoveTowards(value, player.x, player.y, dt)
-    end
-end
+
 function love.update(dt)
     
     --check the player stays withinscreen
@@ -114,8 +85,6 @@ function love.draw()
     love.graphics.print('' .. oldText, 0, 226-14-14)
     love.graphics.print('' .. text, 0, 226-14)
 
-    love.graphics.setColor(255/255, 119/255, 168/255)
-    love.graphics.rectangle('fill', enemy.x, enemy.y, 6,6)
     --draw enemies
     for index, value in ipairs(enemyList) do
         love.graphics.setColor(255/255, 119/255, 168/255)
@@ -124,8 +93,7 @@ function love.draw()
         love.graphics.print(value.word, value.x - 10, value.y -10)
     end
     
-    love.graphics.setColor(241/255, 173/255, 255/255)
-    love.graphics.print(enemy.word, enemy.x - 10, enemy.y -10)
+
     --love.graphics.setColor(love.math.colorFromBytes(5, 234, 255))
 
 
@@ -248,14 +216,38 @@ function SpawnEnemy()
 end
 
 
-
-function drawEnemy(enemy)
-
-end
-
 function love.mousepressed(x, y, button, istouch)
     if button == 1 then -- Versions prior to 0.10.0 use the MouseConstant 'l'
         player.moveToX = maid64.mouse.getX()
         player.moveToY = maid64.mouse.getY()
     end
 end
+
+
+--Adding enemies, used in the upadte function
+function ManageEnemies(dt)
+    local currentTime = love.timer.getTime()
+        local updatedSpawnTime = false
+      
+        -- Spawn enemy every nth second
+        if currentTime - prevSpawnTime >= enemySpawnTimer then
+            print(enemySpawnTimer .. " has passed. Spawn enemy")
+            prevSpawnTime = currentTime
+            SpawnEnemy()
+            updatedSpawnTime = true
+        end
+      
+        -- Decrement spawn time if necessary
+        if enemySpawnTimer > 0.5 and not updatedSpawnTime then
+            if currentTime - prevDecilineTime >= spawnTimeDecliner then
+                prevDecilineTime = currentTime
+                enemySpawnTimer = enemySpawnTimer - 0.5
+                print('Decline spawn time with 0.5, new spawn time: ' .. enemySpawnTimer)
+            end
+        end
+      
+        -- Move enemies towards the player
+        for index, value in ipairs(enemyList) do
+          MoveTowards(value, player.x, player.y, dt)
+        end
+    end
